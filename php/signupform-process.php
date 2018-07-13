f7b1a0b01e0dde0837dc115395658fd617bb0b96
lucasmdo <lucasmdo419@gmail.com>
2018-07-13 15:27:48 -0300

    Initial commit

---
+++ php/signupform-process.php
@@ -0,0 +1,31 @@
+<?php
+$errorMSG = "";
+
+if (empty($_POST["email"])) {
+    $errorMSG = "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+$EmailTo = "emiltartea@gmail.com";
+$Subject = "New newsletter signup from Riga landing page";
+
+// prepare email body text
+$Body = "";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+?>
